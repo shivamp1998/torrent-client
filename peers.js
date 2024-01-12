44ea5d154b654ae17d7979ba1a2e0fe915c779a9
shivamp1998 <shivampandey98@gmail.com>
2024-01-12 22:53:52 +0530

    build announcement request

--- peers.js
+++ peers.js
@@ -4,6 +4,7 @@ import { parse as urlParse } from 'url';
 import crypto from 'crypto';
 import bencode from 'bencode';
 import torrentService from './torrentService.js'
+import misc from './misc.js'
 const sendMessageUDP = (socket, message, rawUrl, callback = () => {}) => {
     const url = urlParse(rawUrl)
     socket.send(message, 0, message.length, url.port, url.host.split(':')[0], callback)
@@ -23,8 +24,18 @@ const buildAnnounceRequest = (connId, torrent, port = 6881) => {
     const buff = Buffer.allocUnsafe(98);
     connId.copy(buff, 0);
     buff.writeUInt32BE(1, 8);
-    crypto.randomBytes(4).copy(buff, 12)
-    buff.write()
+    crypto.randomBytes(4).copy(buff, 12);
+    torrentService.infoHash(torrent).copy(buf, 16);
+    misc.genId().copy(buff, 36);
+    Buffer.alloc(8).copy(buff, 56);
+    torrentService.size(torrent).copy(buff, 64);
+    Buffer.alloc(8).copy(buff, 72);
+    buff.writeUInt32BE(0, 80);
+    buff.writeUInt32BE(0, 80);
+    crypto.randomBytes(4).copy(buff, 88);
+    buff.writeInt32BE(-1, 92);
+    buff.writeInt16BE(port, 96);
+    return buff;    
 }
 
 const responseType = (response) => {
@@ -42,7 +53,7 @@ const parseConnectionResponse = (response) => {
 }
 
 const parseAnnouncementResponse = (response) => {
-
+    
 }
 
 
