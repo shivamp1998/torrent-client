eb08ec483efa6379b1550dd43caeebf4593f69c4
shivamp1998 <shivampandey98@gmail.com>
2024-03-18 21:25:28 +0530

    added message module

--- src/message.js
+++ src/message.js
@@ -1,7 +1,24 @@
 import { Buffer } from 'Buffer'
 import torrentService from './torrentService'
+import util from './misc'
 
-
-const handleHandshake = (torrent) => {
-    const buf = Buffer.alloc(68)
+export const handleHandshake = (torrent) => {
+    const buf = Buffer.alloc(68);
+    buf.writeUInt8(19,0);
+    buf.write('BitTorrent protocol',1);
+    buf.writeUINT32BE(0, 20);
+    buf.writeUINT32BE(0,24);
+    torrentService.infoHash(torrent).copy(buf, 28);
+    buf.write(util.genId());
+    return buf;
 }
+
+
+export const buildKeepAlive = () => Buffer.alloc(4);
+
+export const buildChoke = () => {
+    const buf = Buffer.alloc(5);
+    
+}
+
+
